167ffe12c34aed77d53ed33a50c4b6c3c11bc1d0
aancjr <45945137+aancjr@users.noreply.github.com>
2019-06-13 18:24:33 -0400

    added cli-table3 NPM, created function createTable for visual table of mysql data

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,11 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var cliTable = require("cli-table3");
+//variable to create the table using cliTable
+var table = new cliTable({
+    head: ["item_id", "product_name", "department_name", "price", "stock_quantity"],
+    colWidths: [20, 20, 20, 20, 20]
+});
 
 //connection to mysql database
 var connection = mysql.createConnection({
@@ -17,18 +23,28 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected as ID: " + connection.threadId +"\n");
-    showProducts();
+
+    createTable();
 })
 
-//show products upon app load
-function showProducts() {
-    console.log("Available Products:")
+//function to create a table out of the data in mySQL
+function createTable() {
+    console.log("Available products: ")
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        //showing results from SELECT statement...
-        console.log(res);
+        for (var i = 0; i < res.length; i++) {
+            var tableId = res[i].item_id;
+            var productName = res[i].product_name;
+            var deptName = res[i].department_name;
+            var price = res[i].price;
+            var stockQuan = res[i].stock_quantity;
+            
+            table.push([tableId, productName, deptName, price, stockQuan]
+    ); 
+        }
+        console.log(table.toString());
         inquire();
-    })
+    });
 }
 
 //inquirer to start customer purchasing questions
@@ -46,7 +62,6 @@ function inquire() {
             name: "itemQuantity"
         },
     ]).then(function(user) {
-            console.log("You are looking for: " + user.itemQuantity);
             var idBuy = user.idPurchase;
             var itemCount = user.itemQuantity;
             makePurchase(idBuy, itemCount);
@@ -60,11 +75,13 @@ function makePurchase(idBuy, itemCount) {
         if (err) throw err;
         else if (itemCount <= res[0].stock_quantity) {
             var makePurchase = itemCount*res[0].price;
+            console.log("-------------------");
             console.log("Your final cost is: $" + makePurchase);
             
             connection.query("UPDATE products SET stock_quantity = stock_quantity-" + itemCount + " WHERE item_id = " + idBuy);
             
         } else {
+            console.log("-------------------");
             console.log("Product out of stock! Please pick another product!");
             inquire();
         }
@@ -72,7 +89,9 @@ function makePurchase(idBuy, itemCount) {
     })
 }
 
+//function to prompt the user for their next action - another purchase or leave the store.
 function whatNext () {
+    console.log("\n");
     inquirer.prompt([
         {
             type: "list",
@@ -82,11 +101,15 @@ function whatNext () {
         }
     ]).then(function(user) {
         if (user.next === "Make another purchase") {
+            console.log("\n");
             showProducts();
         } else {
+            console.log("\n");
             connection.end();
         }
     })
 }
+
+
 
 
