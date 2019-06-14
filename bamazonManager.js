c8ab93f3918d8c7aafa06883ffdd0931e632df59
aancjr <45945137+aancjr@users.noreply.github.com>
2019-06-14 19:51:08 -0400

    switch case inquirer, view products, low inventory functions created, and connectionend added where needed

--- bamazonManager.js
+++ bamazonManager.js
@@ -24,8 +24,47 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected as ID: " + connection.threadId +"\n");
 
-    createTable();
+    inquire();
 })
+
+//inquirer to start manager questions
+function inquire() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Leave Store"],
+            name: "askManager"
+        }
+    ]).then(function(user) {
+        var path = user.askManager;
+
+        switch (path) {
+            case "View Products for Sale":
+                return createTable();
+            
+            case "View Low Inventory":
+                return lowInv();
+
+            case "Add to Inventory":
+                return ;
+
+            case "Add New Product":
+                return ;
+            
+            case "Leave Store":
+                console.log("See you next time!")
+                console.log("\n")
+                return connection.end();
+
+            default:
+                console.log("There seems to be an issue, try again.")
+                console.log("\n")
+                return connection.end();
+        }
+        
+        })
+    };
 
 //function to create a table out of the data in mySQL
 function createTable() {
@@ -47,22 +86,23 @@ function createTable() {
     });
 }
 
-//inquirer to start customer purchasing questions
-function inquire() {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Please select the ID of the product you wish to purchase.",
-            name: "idPurchase"
-        },
+function lowInv() {
+    console.log("Low Inventory: ")
+    connection.query("SELECT * FROM products WHERE stock_quantity < 10", function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            var tableId = res[i].item_id;
+            var productName = res[i].product_name;
+            var deptName = res[i].department_name;
+            var price = res[i].price;
+            var stockQuan = res[i].stock_quantity;
+            
+            table.push([tableId, productName, deptName, price, stockQuan]
+    ); 
+        }
+        console.log(table.toString());
+        inquire();
+    } )
+}
 
-        {
-            type: "input",
-            message: "How many items would you like to purchase?",
-            name: "itemQuantity"
-        },
-    ]).then(function(user) {
-            // var idBuy = user.idPurchase;
-            // var itemCount = user.itemQuantity;
-        })
-    };
+
